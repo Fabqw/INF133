7116378cd6126d363a21365267ede18feaf8e157
Fabqw <qferfab27@gmail.com>
2024-02-27 13:42:46 -0400

    adicion de la funcion palindromo

--- soap_server.py
+++ soap_server.py
@@ -9,6 +9,10 @@ def sumar(num1, num2):
     resultado = num1 + num2
     return "La suma de {} y {} es: {}".format(num1, num2, resultado)
 
+def es_palindromo(palabra):
+    palabra = palabra.lower()
+    return palabra == palabra[::-1]
+
 #El que va adespachar el resultado del endpoit
 dispatcher = SoapDispatcher(
     "ejemplo-soap-server",
@@ -20,18 +24,19 @@ dispatcher = SoapDispatcher(
 )
 
 dispatcher.register_function(
+    "Saludar",
+    saludar,
+    returns={"saludo": str},
+    args={"nombre": str},
+)
+
+dispatcher.register_function(
     "Sumar",
     sumar,
     returns={"resultado": str},
     args={"num1": int, "num2": int},
 )
 
-dispatcher.register_function(
-    "Saludar",
-    saludar,
-    returns={"saludo": str},
-    args={"nombre": str},
-)
 
 server = HTTPServer(("0.0.0.0",8000),SOAPHandler)
 server.dispatcher = dispatcher
